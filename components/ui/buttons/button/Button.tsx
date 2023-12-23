446c70203766056c5a19e517d285014010d3d9ad
slanskeeh <maximgerasimenko@bk.ru>
2023-12-23 22:24:38 +0300

    button in work

---
+++ components/ui/buttons/button/Button.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import styles from "./Button.module.scss";
+import { IButton } from "./Button.types";
+
+const Button: React.FC<IButton> = ({ variant, size, color, text, icon }) => {
+  const iconElem =
+    typeof icon === "string" ? (
+      <Image src={icon} width={24} height={24} alt="icon" />
+    ) : (
+      icon
+    );
+
+  return (
+    <button
+      data-size={size}
+      data-color={color}
+      data-variant={variant}
+      className={styles.button}
+    >
+      <div className={styles.button_icon}>{iconElem}</div>
+      <p className="p-bold">{text}</p>
+    </button>
+  );
+};
+
+export default Button;
---
+++ components/ui/buttons/button/Button.types.ts
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+
+export interface IButton {
+  variant: "blur" | "solid" | "alpha";
+  size: "m" | "s";
+  color: "gray" | "orange";
+  text: string;
+  icon: string | ReactNode;
+}
